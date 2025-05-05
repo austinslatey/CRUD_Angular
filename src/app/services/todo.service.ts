9d77f7c6e328a7205b361bc21da6cd3f235ce1ec
Austin Slater <austinslater58@gmail.com>
2025-05-05 20:42:52 -0300

    Add services folder with todo service

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoService {
+  private todos: Todo[] = [
+    { id: 1, title: 'Learn Angular', completed: false },
+    { id: 2, title: 'Build CRUD App', completed: true },
+  ];
+
+  getTodos(): Observable<Todo[]> {
+    console.log('Returning todos:', this.todos); // Debug log
+    return of(this.todos);
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    console.log('Service adding todo:', todo); // Debug log
+    // Prevent duplicates by checking id or title
+    if (!this.todos.some((t) => t.id === todo.id || t.title === todo.title)) {
+      this.todos = [...this.todos, todo]; // Update immutably
+    }
+    return of(todo);
+  }
+
+  updateTodo(updatedTodo: Todo): Observable<Todo> {
+    const index = this.todos.findIndex((todo) => todo.id === updatedTodo.id);
+    if (index !== -1) {
+      this.todos = [
+        ...this.todos.slice(0, index),
+        updatedTodo,
+        ...this.todos.slice(index + 1),
+      ];
+    }
+    return of(updatedTodo);
+  }
+
+  deleteTodo(id: number): Observable<void> {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    return of();
+  }
+}
